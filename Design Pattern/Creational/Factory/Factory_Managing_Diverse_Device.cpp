67772060943879e596fa0d7b110965cd6b953e23
MortezaKhandan <49358240+MortezaKhandan@users.noreply.github.com>
2023-12-19 15:04:33 +0800

    Create Factory_Managing_Diverse_Device.cpp

---
+++ Design Pattern/Creational/Factory/Factory_Managing_Diverse_Device.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <map>
+#include <memory>
+
+class Device {
+public:
+    virtual void connect() = 0;
+};
+
+class Sensor : public Device {
+public:
+    void connect() override {
+        std::cout << "Connecting sensor via I2C protocol..." << std::endl;
+    }
+};
+
+class Printer : public Device {
+public:
+    void connect() override {
+        std::cout << "Connecting printer via Bluetooth..." << std::endl;
+    }
+};
+
+class DeviceFactory {
+public:
+    static std::unique_ptr<Device> createDevice(const std::string& type) {
+        if ( type == "sensor" ) {
+            return std::make_unique<Sensor>();
+        }
+        else if ( type == "printer" ) {
+            return std::make_unique<Printer>();
+        }
+        else {
+            throw std::invalid_argument("Unknown device type: " + type);
+        }
+    }
+};
+
+int main() {
+    // Use factory to create devices
+    auto sensor = DeviceFactory::createDevice("sensor");
+    sensor->connect();
+
+    auto printer = DeviceFactory::createDevice("printer");
+    printer->connect();
+
+    return 0;
+}
